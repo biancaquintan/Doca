2b076ce62a567f0d8454b99fb857ea9ad57b0556
davispc10 <davispc100@gmail.com>
2018-10-15 19:24:09 -0300

    Listagem de Pedidos, filtro por acesso área e filtros de turno e nivel.

--- app/controllers/registro_academicos_controller.rb
+++ app/controllers/registro_academicos_controller.rb
@@ -1,11 +1,11 @@
 class RegistroAcademicosController < ApplicationController
   before_action :authenticate_usuario!
-  before_action :is_registro?
+  before_action :is_registro?  
+  before_action -> { filter_pedido_atestado("situacao_id in (3,4,5)") }, only: [:pedidos]
 
   def index
   end
 
   def pedidos
-    @pedido_atestados = PedidoAtestado.all
   end
 end
