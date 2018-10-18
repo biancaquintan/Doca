c34c602320171664209b280bd244d83ca6418367
bianca_sota <bsquintan@gmail.com>
2018-10-17 21:25:31 -0300

    ajuste de layout form atestado

--- app/assets/javascripts/buscaCurso.js
+++ app/assets/javascripts/buscaCurso.js
@@ -1,19 +1,19 @@
-$(document).on('ready page:load', function () {  
-  $('#curso_id').change(function(event) {    
+$(document).on('ready page:load', function () {
+  $('#curso_id').change(function(event) {
     var select_periodo = $('select#periodo');
     var CursoId = $(event.currentTarget);
     var idPeriodo = document.getElementById("idPeriodo");
-    
+
     $.ajax({
       type:"GET",
       url:"/cursos",
       dataType:"json",
       data: {curso_id: CursoId.val()},
       success:function(result) {
-        select_periodo.empty().append("Selecione");
-        select_periodo.append($('<option>').attr('value', "").text("Selecione"));        
-        select_periodo.attr('disabled', true);          
-        
+        select_periodo.empty().append("Selecione o período/módulo");
+        select_periodo.append($('<option>').attr('value', "").text("Selecione o período/módulo"));
+        select_periodo.attr('disabled', true);
+
         if (result.quantidadePeriodo > 0)
           idPeriodo.style.display = 'block';
         else
@@ -22,7 +22,7 @@ $(document).on('ready page:load', function () {
         for (var i = 1; i <= result.quantidadePeriodo; i++) {
           select_periodo.append($('<option>').attr('value', i).text(i));
           select_periodo.attr('disabled', false);
-        }        
+        }
       }
     });
   });
