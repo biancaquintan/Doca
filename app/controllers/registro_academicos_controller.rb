8265900dbcf4122ac2ac1371f7eab4d158c871ec
davispc10 <davispc100@gmail.com>
2018-11-11 21:17:22 -0200

    Adição do filtro situação na tela de pedidos

--- app/controllers/registro_academicos_controller.rb
+++ app/controllers/registro_academicos_controller.rb
@@ -1,7 +1,7 @@
 class RegistroAcademicosController < ApplicationController
   before_action :authenticate_usuario!
   before_action :is_registro?  
-  before_action -> { filter_pedido_atestado("situacao_id in (2,4,5)") }, only: [:pedidos]
+  before_action -> { filter_pedido_atestado("(2,4,5)") }, only: [:pedidos]
 
   def index
   end
